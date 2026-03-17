fb8892ac4bfa46f6e981f4c7a526d18e0b608e7e
Sung-Kyu Yoo <skyoo2003@gmail.com>
2026-03-18 04:41:04 +0900

    feat(logging): add gRPC interceptor

---
+++ pkg/logging/grpc.go
@@ -0,0 +1,28 @@
+package logging
+
+import (
+	"context"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+)
+
+func GRPCUnaryInterceptor(logger *Logger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		start := time.Now()
+
+		resp, err := handler(ctx, req)
+
+		duration := time.Since(start)
+		st, _ := status.FromError(err)
+
+		logger.Info().
+			Str("method", info.FullMethod).
+			Str("status", st.Code().String()).
+			Int64("latency_ms", duration.Milliseconds()).
+			Msg("request completed")
+
+		return resp, err
+	}
+}
